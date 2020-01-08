a9593195611fa0742643450b2e7fd4e139eb3de2
Michael Burns <55208620+burnsmj@users.noreply.github.com>
2020-01-08 14:20:58 -0600

    Added note at bottom to test
    Added note as a test.

--- Data_Partitioner.R
+++ Data_Partitioner.R
@@ -7,3 +7,5 @@ mup_train<-kernel_comp_data[mup_index,]
 
 write.csv(mup_train, "/Users/michael/Desktop/Grad_School/Research/Datasets/Machine Learning/Matlab_Tests/Matlab_all_nonred_traits_train.csv", row.names = F)
 write.csv(mup_test, "/Users/michael/Desktop/Grad_School/Research/Datasets/Machine Learning/Matlab_Tests/Matlab_all_nonred_traits_test.csv", row.names = F)
+
+#Added this note as a test
